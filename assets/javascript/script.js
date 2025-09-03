3eaabb137f677709df61ed5267bdd8bc73e362e0
clarkey164 <s.clarke0896@gmail.com>
2025-09-03 01:11:15 +0100

    Initial Structure in place for files

---
+++ assets/javascript/script.js
@@ -0,0 +1,26 @@
+// Global Variables
+
+function runGame() {
+    
+} 
+
+function flipCard() {
+    
+}
+
+function checkForMatch() {
+      
+}
+
+function disableCards() {
+    
+}
+
+function unflipCards() {
+    
+}
+
+function resetBoard() {
+    
+}  
+  
